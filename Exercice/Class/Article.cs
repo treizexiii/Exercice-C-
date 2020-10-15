7743f3bf91af087a037017ed56995a2e574d74dd
Treize <darktreize@hotmail.fr>
2020-10-15 22:50:58 +0200

    v1.0

---
+++ Exercice/Class/Article.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercice.Class
+{
+    public class Article
+    {
+        private int _numRef { get; set; }
+        private int _quantity { get; set; }
+        private string _name { get; set; }
+        private double _price { get; set; }
+
+        public int NumRef
+        {
+            get { return _numRef; }
+            set { _numRef = value; }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public double Prix
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+
+        public int Quantite
+        {
+            get { return _quantity; }
+            set { _quantity = value; }
+        }
+
+        public Article (int n, string na, double p, int q)
+        {
+            _numRef = n;
+            _name = na;
+            _price = p;
+            _quantity = q;
+        }
+
+        public override string ToString()
+        {
+            return $"référence : {_numRef}, nom : {_name}, prix : {_price}, quantité : {_quantity}";
+        }
+
+    }
+}
---
+++ Exercice/Program.cs
@@ -0,0 +1,90 @@
+﻿using Exercice.Class;
+using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace Exercice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Article> listArticle = new List<Article>();
+
+            int choice = 0, p, numRef;
+
+            do
+            {
+                Console.WriteLine("menu :");
+                Console.WriteLine("Rechercher un article : ");
+                Console.WriteLine("Ajouter un article : ");
+                Console.WriteLine("Supprimer un article : ");
+                choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Indiquer la référence de l'article recherché :");
+                        numRef = Convert.ToInt32(Console.ReadLine());
+                        p = Rechercher(listArticle, numRef);
+                        if(p == -1)
+                        {
+                            Console.WriteLine("L'article est introuvable.");
+                        }
+                        else
+                        {
+                            Console.WriteLine(listArticle[p].ToString());
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("Saisissez la référence de l'article : ");
+                        numRef = Convert.ToInt32(Console.ReadLine());
+                        p = Rechercher(listArticle, numRef);
+                        if (p != -1)
+                        {
+                            Console.WriteLine("L'article existe déjà");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Saisissez le nom de l'article : ");
+                            string name = Console.ReadLine();
+                            Console.WriteLine("Indiquez le prix : ");
+                            double price = Convert.ToDouble(Console.ReadLine());
+                            Console.WriteLine("Indiquer la quantité d'article en stock : ");
+                            int quantity = Convert.ToInt32(Console.ReadLine());
+                            listArticle.Add(new Article(numRef, name, price, quantity));
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine("Saisissez la référence de l'article à supprimer : ");
+                        numRef = Convert.ToInt32(Console.ReadLine());
+                        p = Rechercher(listArticle, numRef);
+                        if(p == -1)
+                        {
+                            Console.WriteLine("L'article est introuvable.");
+                        }
+                        else
+                        {
+                            listArticle.RemoveAt(p);
+                            Console.WriteLine("L'article a bien été supprimé.");
+                        }
+                        break;
+
+                }
+            } while (choice != 1);
+        }
+
+        static int Rechercher(List<Article>L,int r)
+        {
+            int p = -1;
+            for(int i = 0; i<L.Count; i++)
+            {
+                if(L[i].NumRef == r)
+                {
+                    p = i;
+                    break;
+                }
+            }
+            return p;
+        }
+    }
+}
